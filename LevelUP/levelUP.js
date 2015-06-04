cf8570c7d7d43325afe83970da1a364fdf8cf7d7
AlekseiKlimenko <rad-wam@mail.ru>
2015-06-04 11:26:59 +0300

    commit

---
+++ LevelUP/levelUP.js
@@ -0,0 +1,5 @@
+var result;
+var addClass=document.getElementById('addClass');
+document.querySelector('input[value="addClass"]').addEventListener('click',function(event){
+    result.classList.add('red');
+})
