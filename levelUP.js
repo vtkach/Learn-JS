903b692d6358b9c5daf09c83fd68039bfc381ed3
AlekseiKlimenko <rad-wam@mail.ru>
2015-06-05 11:22:18 +0300

    levelUP

--- levelUP.js
+++ levelUP.js
@@ -1,5 +1,5 @@
-var result;
-var addClass=document.getElementById('addClass');
-document.querySelector('input[value="addClass"]').addEventListener('click',function(event){
-    result.classList.add('red');
+var rest=document.getElementById('test');
+var inputAdd = document.getElementById('addClass');
+inputAdd.addEventListener('click',function(event){
+    test.classList.add('red');
 })
