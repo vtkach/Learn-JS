523d55209cc9e7963ff33ae64cdd78d241ab61f6
AlekseiKlimenko <rad-wam@mail.ru>
2015-08-14 05:47:24 +0300

    1

--- js s.js
+++ js s.js
@@ -1,12 +1,18 @@
-window.onload=function() {
-    var table = document.getElementsByTagName('table');
-var add=document.getElementById('addTable').addEventListener('click',addTabl,false);
-    function addTabl(e){
-        table = Array.prototype.slice.call(table);
-        table.forEach(function(i) {
-            table +=table.childNodes;
-            document.body.appendChild(table)
-        });
+//Создать кнопку, по нажатию на которую, появится копия исходной таблицы, но с порядком полей: 1, 3, 2.
+//Применить для новой таблицы стили t2
 
-    }
-}
+window.onload = function (){
+    var table = document.getElementsByTagName('table')[0],
+        copy_table,
+        addTable = document.getElementById('addTable').addEventListener('click',function(){
+            copy_table = table.cloneNode(true),
+            [].forEach.call(copy_table.rows,function(i){
+                var row = i;
+                var tmp = row.cells[1].innerHTML;
+                row.cells[1].innerHTML=row.cells[2].innerHTML;
+                row.cells[2].innerHTML=tmp;
+                document.body.appendChild(copy_table);
+                copy_table.classList.add('t2');
+                })
+        },false);
+};
